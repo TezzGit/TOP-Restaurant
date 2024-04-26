03b6695d2fd8b4a8db33d4aad9c0472742841092
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-04-26 12:38:17 +1000

    Add Website About Data into HTML

--- src/about.js
+++ src/about.js
@@ -13,11 +13,71 @@ function About() {
 
     const contentDiv = document.body.querySelector('#content');
     contentDiv.innerHTML = "";
+    
     const mainContent = document.createElement("div");
     mainContent.classList.add('main-content');
-    mainContent.innerHTML = "About"
-    contentDiv.appendChild(mainContent);
+    mainContent.classList.add('about-content');
+    
+    const aboutDiv = document.createElement("div");
+    aboutDiv.classList.add("about-statement");
+    aboutDiv.innerHTML = `${aboutData.content.about}`
+    mainContent.appendChild(aboutDiv);
 
+    const locationsDiv = document.createElement("div");
+    locationsDiv.classList.add("locations");
+    const locations = [...aboutData.content.loc_info];
+
+    locations.forEach((element) => {
+        const location = document.createElement("div");
+        location.classList.add("location");
+
+        const locName = document.createElement("div");
+        locName.classList.add("location-name");
+        locName.innerHTML = `${element.loc_name}`;
+        location.appendChild(locName);
+
+        const locAdd = document.createElement("div");
+        locAdd.classList.add("address");
+        locAdd.innerHTML = `${element.loc_add}`;
+        location.appendChild(locAdd);
+
+        const locPhone = document.createElement("div");
+        locPhone.classList.add("phone-numner");
+        locPhone.innerHTML = `${element.loc_phone}`;
+        location.appendChild(locPhone);
+
+        const locEmail = document.createElement("div");
+        locEmail.classList.add("email");
+        locEmail.innerHTML = `${element.loc_email}`;
+        location.appendChild(locEmail);
+
+        const tradingDiv = document.createElement("div");
+        tradingDiv.classList.add("trading");
+        const hours = [...element.trading];
+
+        hours.forEach((subElem)=> {
+            const dayDiv = document.createElement("div");
+            dayDiv.classList.add("trading-day");
+            
+            const dayName = document.createElement("div");
+            dayName.classList.add("day-name");
+            dayName.innerHTML = `${subElem.day}`;
+            dayDiv.appendChild(dayName);
+
+            const dayHours = document.createElement("div");
+            dayHours.classList.add("hours");
+            dayHours.innerHTML = `${subElem.hours}`;
+            dayDiv.appendChild(dayHours);
+
+            tradingDiv.appendChild(dayDiv);
+        })
+
+        location.appendChild(tradingDiv);
+
+        locationsDiv.appendChild(location);
+    })
+    mainContent.appendChild(locationsDiv);
+    contentDiv.appendChild(mainContent);
 }
 
 export default About;
